4ef3d620e55684e44915fa8d414e798d7cc9d486
Siddharth Mandgi <siddh30@gmail.com>
2021-06-20 01:55:47 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,22 +1,10 @@
 import streamlit as st
-from selenium import webdriver
 from time import sleep
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.options import Options
 import os
 import pandas as pd
 
 from PIL import Image
 
-# def capture(txt):
-#     options = webdriver.ChromeOptions()
-#     options.add_argument('--headless')
-#     options.add_argument('--start-maximized')
-#     driver = webdriver.Chrome(ChromeDriverManager().install(),chrome_options=options)
-#     driver.get(txt)
-#     driver.get_screenshot_as_file("capture.png")
-#     # st.image('capture.png', use_column_width=500)
-#     driver.quit()
 
 
 im = Image.open("A.jpg")
@@ -52,17 +40,3 @@ st.sidebar.image('coVID-19.png', use_column_width=50)
 st.sidebar.success("Wear a mask!")
 st.sidebar.warning("Wash your hands!")
 st.sidebar.error("Maintain Social Distancing!")
-# def capture(txt):
-#     options = webdriver.ChromeOptions()
-#     options.add_argument('--headless')
-#     options.add_argument('--start-maximized')
-#     driver = webdriver.Chrome(ChromeDriverManager().install(),chrome_options=options)
-#     driver.get(txt)
-#     driver.get_screenshot_as_file("capture.png")
-#     st.image('capture.png', use_column_width=500)
-#     driver.quit()
-
-
-
-# capture('https://www.google.com/search?rlz=1C5CHFA_enUS877US877&ei=0dSuX5vjM6-RggeS1464DA&q=covid+count&oq=covid+count&gs_lcp=CgZwc3ktYWIQAzILCAAQsQMQgwEQyQMyBQgAEJIDMgUIABCSAzIICAAQsQMQgwEyCAgAELEDEIMBMggIABCxAxCDATICCAAyAggAMggIABCxAxCDATIICAAQsQMQgwE6BAgAEEc6BAgAEEM6CggAELEDEIMBEEM6BwgAELEDEEM6CAgAELEDEMkDOg4IABCxAxCDARDJAxCRAjoFCAAQsQNQghVYrCVg6ydoAHADeACAAYICiAHVB5IBBTIuMi4ymAEAoAEBqgEHZ3dzLXdpesgBBsABAQ&sclient=psy-ab&ved=0ahUKEwjbj_y2l4DtAhWviOAKHZKrA8cQ4dUDCA0&uact=5')
-# os.remove('capture.png')
